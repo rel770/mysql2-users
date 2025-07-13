b6c3baf20a8998b38a5ea26349d8026c23479af2
Ariel <arieltanami122@gmail.com>
2025-07-13 11:22:55 +0300

    Update root route ('/') to return available API endpoints

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,16 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World! MySQL Server is running!");
+  res.json({
+    endpoints: [
+      { method: "GET", path: "/", description: "Welcome message" },
+      { method: "GET", path: "/users", description: "Get all users" },
+      { method: "GET", path: "/users/:id", description: "Get user by ID" },
+      { method: "POST", path: "/users", description: "Create new user" },
+      { method: "PUT", path: "/users/:id", description: "Update user" },
+      { method: "DELETE", path: "/users/:id", description: "Delete user" }
+    ]
+  });
 });
 
 export default router;
