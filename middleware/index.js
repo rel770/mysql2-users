9ba7fb0244f859632bf174bb1e0839f40fbd32d1
Ariel <arieltanami122@gmail.com>
2025-07-14 15:02:34 +0300

    Add middleware index file for central exports

---
+++ middleware/index.js
@@ -0,0 +1,8 @@
+// Middleware index file - central export point
+export * from './errorMiddleware.js';
+export * from './validationMiddleware.js';
+export * from './commonMiddleware.js';
+
+// Add more middleware exports here as your app grows
+// export * from './authMiddleware.js';
+// export * from './rateLimitMiddleware.js';
