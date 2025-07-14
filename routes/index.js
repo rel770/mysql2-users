6a5d7df9479624da0fe616cea07324be58191dd8
Ariel <arieltanami122@gmail.com>
2025-07-14 15:05:09 +0300

    Refactor index router to use appController (healthCheck & getApiInfo functions)

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 import express from "express";
+import { healthCheck, getApiInfo } from "../controllers/appController.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    endpoints: [
-      { method: "GET", path: "/", description: "Welcome message" },
-      { method: "GET", path: "/users", description: "Get all users" },
-      { method: "GET", path: "/users/:id", description: "Get user by ID" },
-      { method: "POST", path: "/users", description: "Create new user" },
-      { method: "PUT", path: "/users/:id", description: "Update user" },
-      { method: "DELETE", path: "/users/:id", description: "Delete user" }
-    ]
-  });
-});
+// Health check endpoint
+router.get("/", healthCheck);
+
+// API info endpoint
+router.get("/info", getApiInfo);
 
 export default router;
