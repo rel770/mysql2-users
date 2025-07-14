bbc7b984bd55dca38a63d0c5d596086c3dcd0e91
Ariel <arieltanami122@gmail.com>
2025-07-14 14:52:11 +0300

    Add controllers index file for central exports

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+// Controllers index file - central export point
+export * from './userController.js';
+export * from './appController.js';
+
+// Add more controllers here in the future, for example:
+// export * from './authController.js';
+// export * from './adminController.js';
